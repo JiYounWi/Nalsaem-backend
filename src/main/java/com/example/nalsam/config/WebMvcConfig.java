c26a2fa029e28c087f495e1d829c0918e6b63870
WiJiYoun <wi49325@gmail.com>
2023-11-30 02:08:02 +0900

    second commit

--- src/main/java/com/example/nalsam/config/WebMvcConfig.java
+++ src/main/java/com/example/nalsam/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://d3vujzdjaml09b.cloudfront.net/")
+                .allowedOrigins("http://localhost:3000","https://d15y1hflvb5egs.cloudfront.net/","https://udegaforyou.com/")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
 
     }
